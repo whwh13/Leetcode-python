d5088b90c1457ddc5d677e682bcf07403fe14a39
whwh13 <1764087997@qq.com>
2024-09-25 03:14:23 +0000

    迁移codespace到东南亚

--- 16.3sum-closest.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-# 没有写完
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        n=len(nums)
-        nums.sort()
-        ans=nums[0]+nums[1]+nums[2]
