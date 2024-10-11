7958a0c728bf89e9cfc24b59c51082fa5c5948a9
whwh13 <1764087997@qq.com>
2024-10-11 02:19:04 +0000

    2024.10.11 不想看文献，刷leecode都像在摸鱼

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-ans = []
-k = 1
-last0 = "()()()"
-ans.append('('+last0[:k]+')'+last0[k:])
-print(ans)
+import heapq
+
+# 创建一个空堆
+heap = []
+
+# 向堆中添加元素
+heapq.heappush(heap, 1)
+heapq.heappush(heap, 3)
+heapq.heappush(heap, 2)
+heapq.heappush(heap, 5)
+heapq.heappush(heap, 4)
+
+# 打印堆
+print("Heap:", heap)
+
+# 弹出堆中的最小元素
+print("Popped:", heapq.heappop(heap))
+
+# 打印弹出元素后的堆
+print("Heap after pop:", heap)
